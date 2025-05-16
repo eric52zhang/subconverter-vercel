08af6eba17fbb2d23e36470d167a103b61982351
eric52zhang <146424840+eric52zhang@users.noreply.github.com>
2025-05-16 12:56:58 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,85 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "./js/[name].js"
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'js/[name].[contenthash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    mode: process.env.NODE_ENV,
-    // devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node-moudles/
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, {
-                    loader: "css-loader",
-                    options: {
-                        url: false
-                    }
-                }],
-                exclude: /node-moudles/
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            url: false
-                        }
-                    },
-                    "less-loader"
-                ],
-                exclude: /node-moudles/
-            }
-        ]
-    },
-    plugins: [
-        // new BundleAnalyzerPlugin(),
-        new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/[name].css"
-        }),
-        new CSSMinimizerPlugin(),
-        new NodePolyfillPlugin(),
-        new CopyPlugin({
-            patterns: [
-                { from: "public/favicon.ico", to: "favicon.ico" }
-            ],
-        })
-    ],
-    resolve: {
-        extensions: [".ts", "..."]
-    },
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-            cacheGroups: {
-                vendors: {
-                    test: /node_modules/,
-                    filename: "js/chunk-vendor.[name].js",
-                }
-            }
+  },
+  devtool: isProduction ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash:8][ext]',
         },
-        minimize: true,
-        minimizer: [new TerserPlugin({
-            terserOptions: {
-                toplevel: true
-                
-            },
-            extractComments: false
-        })]
-    }
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true,
+  },
 };
